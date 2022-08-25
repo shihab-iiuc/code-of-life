b43ceabb163190edbc8539d5ef1242ab4f8d761a
shihab-iiuc <109750541+shihab-iiuc@users.noreply.github.com>
2022-08-26 02:33:04 +0600

    Create pattern 7.cpp

---
+++ pattern 7.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N,i,j,k,l,m;
+    cin>>N;
+    for( i=1;i<=N;i++)
+    {
+        for(j=N-i;j>=1;j--)
+          {
+              cout<<" ";
+          }
+          
+         
+         for( k=1;k<=2*i-1;k++)
+         {
+             cout<<"*";
+         }
+        cout<<endl;
+       
+         
+          
+    }
+     for(l=N-1;l>=1;l--)
+        {
+         for(j=N-l;j>=1;j--)
+          {
+              cout<<" ";
+          }
+          
+         
+         for( k=1;k<=2*l-1;k++)
+         {
+             cout<<"*";
+         }
+          cout<<endl;
+            
+        }
+  
+    return 0;
+}
